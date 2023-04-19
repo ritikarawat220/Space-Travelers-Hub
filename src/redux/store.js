def4e030abdf5b49a71665efbe8dd8d9fe1f42a6
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-04-19 22:45:22 +0530

    ADD:Rockets on browser

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import missionReducer from './mission/missionSlice';
+import rocketReducer from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: combineReducers({
     missions: missionReducer,
+    rockets: rocketReducer,
   }),
 });
 
