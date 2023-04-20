16703513d5184c83235468fc524d068bd10edfe2
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-20 10:35:23 +0530

    Add Active-member/ Leave-mission

--- src/components/missionLabel.js
+++ src/components/missionLabel.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import './missionLabel.css';
+import cn from 'classnames';
+import styles from '../styles/missionLabel.module.css';
 
-function MissionLabel({ isReserved }) {
+function MissionLabel({ isReserved, onClick }) {
+  const buttonClassName = cn({
+    joinMission: !isReserved,
+    leaveMission: isReserved,
+  });
+
+  const labelBackgroundColor = isReserved ? '#18a2b8' : '#6d757d';
+  const labelText = isReserved ? 'Active Member' : 'NOT A MEMBER';
+  const buttonText = isReserved ? 'Leave Mission' : 'Join Mission';
+
   return (
-    <div className="label">
-      <div
-        style={{ backgroundColor: isReserved ? '#18a2b8' : '#6d757d' }}
-      >
-        {isReserved ? 'Active Member' : 'NOT A MEMBER'}
-      </div>
-      <button
-        type="button"
-        className={classNames(
-          isReserved ? 'leaveMission' : 'joinMission',
-        )}
-      >
-        {isReserved ? 'Leave Mission' : 'Join Mission'}
+    <div className={styles.label} style={{ backgroundColor: labelBackgroundColor }}>
+      {labelText}
+      <button type="button" className={buttonClassName} onClick={onClick}>
+        {buttonText}
       </button>
     </div>
   );
@@ -25,6 +25,7 @@ function MissionLabel({ isReserved }) {
 
 MissionLabel.propTypes = {
   isReserved: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default MissionLabel;
