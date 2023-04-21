4a721f82f5b62ad1e3a4fd2d43191843de8a3494
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-21 12:30:21 +0530

    Update fetchMissions action

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/head';
 import Rockets from './Routes/rockets';
 import Missions from './Routes/missions';
 import Myprofile from './Routes/myprofile';
 
-import { fetchMissions } from './redux/mission/missionSlice';
-
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchMissions());
-  }, [dispatch]);
-
   return (
     <>
       <div className="App">
--- src/Routes/missions.js
+++ src/Routes/missions.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import MissionTable from '../components/missionTable';
+import { fetchMissions } from '../redux/mission/missionSlice';
 
 function Missions() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
   return (
     <main>
       <MissionTable />
